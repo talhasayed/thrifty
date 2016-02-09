1b885a48823cb99e8ad9f6ae78aeb18c252e5d74
Talha Sayed <talha_sayed@hotmail.com>
2016-02-09 23:04:52 +0300

    Update ThriftyModels

---
+++ ThriftyWeb/Models/ThriftyModels.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThriftyWeb.Models
+{
+    public enum AccountCategory
+    {
+        NotSet = 0,
+        Real,
+        Personal,
+        Nominal
+    }
+
+    public enum TransactionLegType
+    {
+        NotSet = 0,
+        Credit,
+        Debit
+    }
+
+
+    public class Account
+    {
+        public Account()
+        {
+            Id = Guid.NewGuid();
+            AccountCategory = AccountCategory.Real;
+        }
+
+        public Guid Id { get; set; }
+        public string AccountName { get; set; }
+
+        [DefaultValue(AccountCategory.Real)]
+        [Range(1, 3), Display(Name = "Account Category")]
+        public AccountCategory AccountCategory { get; set; }
+    }
+
+    public class Transaction
+    {
+        public Transaction()
+        {
+            Id = Guid.NewGuid();
+            Timestamp = DateTime.Now;
+        }
+
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public virtual ICollection<TransactionLeg> TransactionLegs { get; set; }
+    }
+
+    public class TransactionLeg
+    {
+        public TransactionLeg()
+        {
+            Id = Guid.NewGuid();
+            Timestamp = DateTime.Now;
+        }
+
+        public Guid Id { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public virtual Transaction Transaction { get; set; }
+        public virtual Account Account { get; set; }
+
+        [DefaultValue(TransactionLegType.Credit)]
+        [Range(1, 2), Display(Name = "Transaction Leg Type")]
+        public TransactionLegType TransactionLegType { get; set; }
+    }
+}
